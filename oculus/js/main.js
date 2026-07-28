1122138430482a396d94845da5acca56daa070e9
Ruslan <rustik2989@gmail.com>
2026-07-28 22:25:53 +0700

    added oculus

---
+++ oculus/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach((el) => {
+  el.addEventListener('click', open);
+});
+
+function open(event) {
+  const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach((item) => {
+    item.classList.remove('tabs__btn-item--active');
+  });
+
+  tabContent.forEach((item) => {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  tabTarget.classList.add('tabs__btn-item--acitve');
+  document
+    .querySelector(`#${button}`)
+    .classList.add('tabs__content-item--active');
+}
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+});
